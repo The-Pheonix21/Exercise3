4e14f92b855767066601ef8025cc0ea40a84524f
Chris <c5keith90@gmail.com>
2013-09-04 19:25:54 -0600

    Added updades to exersise 3
    dhfjkankljfc

---
+++ Exercise3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Exercise3 {
+	public static void main(String[] args) {
+		double subterm=0.0;
+		double g;
+		int ns=2;
+		double x;
+		double i;
+		int result;
+		double g;
+		double f = 1.0e-12;
+		Scanner scan= new Scanner (System.in);
+		System.out.println("Please input an integer:");
+		String input = scan.nextLine();
+		g = scan.nextDouble();
+	}
+		g = 1+x;
+		while (subterm > f) {
+		    subterm=mymath( double x, double ns);
+			ns++;
+			g += subterm;
+			}
+		System.out.println(Math.exp(x));
+	}
+
+ 	public static void mymath(double x ,double i){
+  	return x^i/factorial(i);
+ 	}
+ 	public static int factorial(int f){
+   	for (int i = 2; i <=f; i++){
+    result *=i;
+   	}
+   	return int result;
+ 
+	
+}
