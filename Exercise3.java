74eb28027b2541ae25bf04292800d6fc4530e113
Chris <c5keith90@gmail.com>
2013-09-05 06:15:46 -0600

    Hi patrick

--- Exercise3.java
+++ Exercise3.java
@@ -1,37 +1,39 @@
 import java.util.Scanner;
-import java.lang.Math;
-class Exercise3 {
+import java.lang.*;
+class Exercise3  {
 	public static void main(String[] args) {
-		double subterm=0.0;
-		double g;
-		int ns=2;
-		double x;
-		double i;
-		int result;
-		double g;
-		double f = 1.0e-12;
-		Scanner scan= new Scanner (System.in);
-		System.out.println("Please input an integer:");
-		String input = scan.nextLine();
-		g = scan.nextDouble();
-	}
-		g = 1+x;
-		while (subterm > f) {
-		    subterm=mymath( double x, double ns);
-			ns++;
-			g += subterm;
-			}
-		System.out.println(Math.exp(x));
-	}
+		 double subterm=1.0;
+		 double g;
+		 int n=2;
+		 double f = 1.0e-12;
+		 double x=5.0;
+		  // Scanner myinput = new Scanner (System.in);
+		  // System.out.println("Please input an integer:");
+		  // x = myinput.nextDouble();
+		  g = 1 + x;
+		   while (subterm > f) {
+		       subterm=mypower(x,n)/factorial(n);
+		       System.out.println("N=" + n + " mypower="+mypower(x,n)+" factorial="+factorial(n));
+		  	 g += subterm;
+		  	 System.out.println("N=" + n + " g=" + g);
+		  	  n++;
+		  	 }
 
- 	public static void mymath(double x ,double i){
-  	return x^i/factorial(i);
- 	}
- 	public static int factorial(int f){
-   	for (int i = 2; i <=f; i++){
-    result *=i;
-   	}
-   	return int result;
- 
-	
+         System.out.println(Math.exp(x) + " " + g);
+		 double text = mypower(x, n);
+	}
+  public static double mypower(double m, int j){
+  			double result=1.0;
+  	for (int i = 1; i<=j; i++){
+  		result = result * m;
+  	}
+   return result;
+  }
+  public static double factorial (int j){
+  	double result = 1.;
+    for (int i = 1; i<=j; i++){
+     result = result * i;
+    }
+    return result;
+  }
 }
